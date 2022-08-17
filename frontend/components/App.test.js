9281b1c2385090453e4a0abb3d56890cc7e713d6
unknown <cliff.reinoso98@yahoo.com>
2022-08-17 01:37:43 -0400

    Completed testing

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,45 @@
-// Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
+import React from 'react'
+import { render, fireEvent, screen } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
+
+import AppFunctional from "./AppFunctional"
+
+
+beforeEach(()=>{
+  render(<AppFunctional />)
 })
+
+describe('<AppFunctional />', () => {
+  test("Looking to see if button#up exists", () => {
+    const Heading3 = screen.getByText("UP") 
+    expect(Heading3).toBeInTheDocument()
+    expect(Heading3).toBeVisible()
+  })
+
+  test("Looking to see if h3#coordinates exists", () => {
+    const Heading3 = screen.getByText("You moved 0 times") 
+    expect(Heading3).toBeInTheDocument()
+    expect(Heading3).toBeVisible()
+  })
+
+  test("Looking to see if button#right exists", () => {
+    const Heading3 = screen.getByText("RIGHT") 
+    expect(Heading3).toBeInTheDocument()
+    expect(Heading3).toBeVisible()
+  })
+
+  test("Looking to see if button#reset exists", () => {
+    const Heading3 = screen.getByText("reset") 
+    expect(Heading3).toBeInTheDocument()
+    expect(Heading3).toBeVisible()
+  })
+
+  test("Looking to see if button#left exists", () => {
+    const Heading3 = screen.getByText("LEFT") 
+    expect(Heading3).toBeInTheDocument()
+    expect(Heading3).toBeVisible()
+  })
+
+})
+
+
--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -22,7 +22,6 @@ export default class AppClass extends React.Component {
   constructor(props) {
     super(props)
     this.state = initialState
-    this.submits = []
   }
 
   reset = () => {
